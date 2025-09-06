91bf147b1ccaa1c699f0076def68fe086f6fc909
Aanis Ali Shah <aanisalishah@gmail.com>
2025-09-07 04:07:30 +0530

    added moe

--- transformer.py
+++ transformer.py
@@ -14,6 +14,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 n_embd = 384
 n_head = 6
 n_layer = 6
+num_experts = 8
+top_k = 2
 dropout = 0.2
 vocab_size = 5000
 weight_decay = 1e-2
@@ -103,7 +105,7 @@ class MultiHeadAttention(nn.Module):
 		out = self.dropout(self.proj(out))
 		return out
 
-# FeedForward
+# FeedForward -> Expert
 class FeedForward(nn.Module):
 
 	def __init__(self, n_embd):
@@ -118,19 +120,92 @@ class FeedForward(nn.Module):
 	def forward(self, x):
 		return self.net(x)
 
+# Top k Router
+class NoisyTopkRouter(nn.Module):
+
+	# Top K Gating with Noise
+	def __init__(self, n_embd, num_experts, top_k):
+		super().__init__()
+		self.top_k = top_k
+
+		# logits router layer
+		self.topkrouter_linear = nn.Linear(n_embd, num_experts)
+		self.noise_linear = nn.Linear(n_embd, num_experts)
+
+	def forward(self, mh_output):
+		logits = self.topkrouter_linear(mh_output)
+		noise_logits = self.noise_linear(mh_output)						# Linear Noise Logits MultiHeadAttentionO/P -> I/P
+
+		# Gaussian Noise
+		noise = torch.randn_like(logits) * F.softplus(noise_logits)
+		noisy_logits = logits + noise
+
+		top_k_logits, indices = noisy_logits.topk(self.top_k, dim=-1)
+		zeros = torch.full_like(noisy_logits, float('-inf'))
+		sparse_logits = zeros.scatter(-1,  indices, top_k_logits)
+		router_output = F.softmax(sparse_logits, dim=-1)
+		return router_output, indices
+
+# MoE Block
+class MOE(nn.Module):
+	
+	def __init__(self, n_embd, num_experts, top_k, capacity_factor=1.0):
+		super().__init__()
+		self.router = NoisyTopkRouter(n_embd, num_experts, top_k)
+		self.experts = nn.ModuleList([FeedForward(n_embd) for _  in range(num_experts)])
+		self.top_k = top_k
+		self.capacity_factor = capacity_factor
+		self.num_experts = num_experts
+
+	def forward(self, x):
+		batch, seq_len, _ = x.shape					# Batching for capacity factor
+		gate_output, indices = self.router(x)
+		final_output = torch.zeros_like(x)
+
+		# Reshape
+		flat_x = x.view(-1, x.size(-1))
+		flat_gate_output = gate_output.view(-1, gate_output.size(-1))
+
+		tokens_per_batch = batch_size * seq_len * self.top_k
+		expert_capacity = int((tokens_per_batch / self.num_experts) * self.capacity_factor)
+
+		updates = torch.zeros_like(flat_x)
+
+		# Data Parallel
+		for i, expert in enumerate(self.experts):
+			expert_mask = (indices == i).any(dim=-1)
+			flat_mask = expert_mask.view(-1)
+			selected_indices = torch.nonzero(flat_mask).squeeze(-1)
+
+			limited_indices = selected_indices[:expert_capacity] if selected_indices.numel() > expert_capacity else selected_indices
+			if limited_indices.numel():
+				expert_input = flat_x[limited_indices]
+				expert_output = expert(expert_input)
+
+				gating_scores = flat_gate_output[limited_indices, i].unsqueeze(1)
+				weighted_output = expert_output * gating_scores
+
+				updates.index_add_(0, limited_indices, weighted_output)
+
+		final_output += updates.view(batch, seq_len, -1)			# Summing up, reshape
+		
+		return final_output
+
 # Block
 class Block(nn.Module):
-	def __init__(self, n_embd, n_head):
+	
+	def __init__(self, n_embd, n_head, num_experts, top_k):
 		super().__init__()
 		head_size = n_embd // n_head
 		self.sa = MultiHeadAttention(n_head, head_size)
-		self.ffwd = FeedForward(n_embd)
+		# self.ffwd = FeedForward(n_embd)
+		self.moe = MOE(n_embd, num_experts, top_k)
 		self.ln1 = nn.LayerNorm(n_embd)
 		self.ln2 = nn.LayerNorm(n_embd)
 
 	def forward(self, x):
 		x = x + self.sa(self.ln1(x))
-		x = x + self.ffwd(self.ln2(x))
+		x = x + self.moe(self.ln2(x))
 		return x
 
 # GPT Neural Network
@@ -140,7 +215,7 @@ class PicoGPTLanguageModel(nn.Module):
 		super().__init__()
 		self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
 		self.position_embedding_table = nn.Embedding(block_size, n_embd)
-		self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+		self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head, num_experts=num_experts, top_k=top_k) for _ in range(n_layer)])
 		self.ln_f = nn.LayerNorm(n_embd) 						# Final LayerNormalizer
 		self.lm_head = nn.Linear(n_embd, vocab_size) 			# Linear Model Heading
 
@@ -173,11 +248,11 @@ class PicoGPTLanguageModel(nn.Module):
 			loss = F.cross_entropy(logits, targets)
 		return logits, loss
 
-	def generate(self, idx, max_new_tokens):
+	def generate(self, idx, max_new_tokens, temperature=1.0):
 		for _ in range(max_new_tokens):
 			idx_contd = idx[:, -block_size:]
 			logits, loss = self(idx_contd) 						# predictions
-			logits = logits[:, -1, :] 							# (B, T) -> (B, C)
+			logits = logits[:, -1, :] / temperature 			# (B, T) -> (B, C)
 			probs = F.softmax(logits, dim=-1)					# (B, C)
 			idx_next = torch.multinomial(probs, num_samples=1)	# (B, 1)
 			idx = torch.cat((idx, idx_next), dim=1)				# (B, T+1)
@@ -209,6 +284,16 @@ for iter in range(max_iters):
 	loss.backward()
 	optimizer.step()
 
-# Generate Text
+# Generating Text
+# Generate random text
 idx = torch.zeros((1,1), dtype=torch.long, device=device)
-print(decode(m.generate(idx, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(idx, max_new_tokens=500, temperature=1.0)[0].tolist()))
+
+# Generate with a prompt
+def generate_response(prompt, max_new_tokens=500, temperature=1.0):
+    idx = torch.tensor(encode(prompt), dtype=torch.long, device=device).unsqueeze(0) # add prompt as a starting point for text generation
+    gen_idx = model.generate(idx, max_new_tokens, temperature=temperature)
+    response = decode(gen_idx[0].tolist())
+    return response
+
+print(generate_response("Hello world, This is a sample text."))
